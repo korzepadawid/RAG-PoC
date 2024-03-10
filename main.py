d62d3d21e4ab2ded74042aaa240b1d53f7baa88a
Dawid Korzepa <56738926+korzepadawid@users.noreply.github.com>
2024-03-10 14:50:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main() -> None:
         mongo_uri=os.getenv('MONGO_DB_URI'),
     )
     openai_llm = OpenAILLM(config=rag_config)
-    print(openai_llm.answer("Which database Meta did extend?"))
+    print(openai_llm.answer("Which database did Meta extend?"))
 
 
 if __name__ == '__main__':
